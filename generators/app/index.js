6f6c3f2977d2f11f518536bc59450ff7aa943d89
Rémy Greinhofer <remy.greinhofer@gmail.com>
2018-07-17 21:19:08 -0500

    Add prompting

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,22 @@
 const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }, {
+      type    : 'confirm',
+      name    : 'cool',
+      message : 'Would you like to enable the Cool feature?'
+    }]).then((answers) => {
+      this.log('app name', answers.name);
+      this.log('cool feature', answers.cool);
+    });
+  }
+
   method1() {
     this.log('method 1 just ran');
   }
